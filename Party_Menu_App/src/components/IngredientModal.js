b481816d7df6ddf79151866f4d2f2101dac1baa2
PILLAGOVLA MAHESH <137051507+PillagovulaMahesh@users.noreply.github.com>
2025-09-13 11:41:26 +0530

    Update IngredientModal.js

---
+++ Party_Menu_App/src/components/IngredientModal.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function IngredientModal({ dish, onClose }) {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <h2>{dish.name}</h2>
+        <p>{dish.description}</p>
+
+        <h4>Ingredients:</h4>
+        <ul>
+          {dish.ingredients.map((ing, idx) => (
+            <li key={idx}>
+              {ing.name} – {ing.quantity}
+            </li>
+          ))}
+        </ul>
+
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+}
+
+export default IngredientModal;
